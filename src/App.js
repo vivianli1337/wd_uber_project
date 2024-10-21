891d503999ceb823d8bdc94d536307468b3b3bfe
vivianli1337 <vivianli1337@gmail.com>
2024-10-20 22:51:19 -0400

    foundation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; 
+
+import { HashRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <HashRouter>
+
+    </HashRouter>
+  );
+}
+
+export default App;
